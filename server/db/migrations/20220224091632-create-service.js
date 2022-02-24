daef800f927038fb140f558d13511993ec302006
Anna <anna.si4eva3000@gmail.com>
2022-02-24 14:22:46 +0300

    Create DB

---
+++ server/db/migrations/20220224091632-create-service.js
@@ -0,0 +1,35 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Services', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Services');
+  },
+};
