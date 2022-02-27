638e0e70bd7adb77ffa8791c857d3067d72cca09
Anna <anna.si4eva3000@gmail.com>
2022-02-27 12:36:24 +0300

    Error

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     dataOut: {
       allowNull: false,
-      tysummape: DataTypes.DATE,
+      type: DataTypes.DATE,
     },
     summa: {
       allowNull: false,
