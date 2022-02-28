b0e5d642a12e9ab6d7dede0879491c980767838a
Tkeshick <dianatkesh@gmail.com>
2022-02-28 16:45:10 +0300

    get img for homes from db

--- client/src/components/HomesCard/HomesCard.jsx
+++ client/src/components/HomesCard/HomesCard.jsx
@@ -3,14 +3,21 @@ import { useDispatch, useSelector } from 'react-redux'
 
 
 
-export const HomesCard = ({homes}) => {
+export const HomesCard = ({ homes }) => {
+  const { imagesHomes } = useSelector(state => state.homesReducer.homes)
+
+  // console.log(imagesHomes);
+  // const curHomeImages = []
+
+  // console.log(homes.id);
 
   return (
-    <>
-    <div>{homes?.description}</div>
-    <div>{homes?.price}</div>
-    {/* <div>{homes?.chips[0].map(el=> <h3>{el}</h3>)}</div> */}
-    </>
+    <div className='homesCard_box'>
+
+      <div>{homes?.description}</div>
+      <div>{homes?.price}</div>
+      {/* <div>{homes?.chips[0].map(el=> <h3>{el}</h3>)}</div> */}
+    </div>
   );
 };
 
