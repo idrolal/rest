c04cf1585e834df67175798bf62e07310c3087d8
Victor Semin <syominvictor@gmail.com>
2022-02-28 17:59:58 +0300

    resolvconf1

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -10,6 +10,7 @@ import AdminCreateReservation from '../AdminComponents//AdminCreateReservation/A
 import AdminAllReservations from '../AdminComponents//AdminAllReservations/AdminAllReservations.jsx';
 import AdminAllHouses from '../AdminComponents//AdminAllHouses/AdminAllHouses.jsx';
 import Nav from '../Nav/Nav';
+import './App.css'
 
 import AdminLogin from '../AdminComponents/AdminLogin/AdminLogin';
 import { HomesList } from '../HomesList/HomesList';
@@ -22,6 +23,7 @@ import Booking2 from '../Booking/Booking2';
 import AdminLogout from '../AdminComponents/AdminLogout/AdminLogout';
 import NotFound from '../NotFound/NotFound';
 import { AdminEditHouseCard } from '../AdminComponents/AdminEditHouseCard/AdminEditHouseCard';
+import AdminEditReservations from '../AdminComponents/AdminEditReservations/AdminEditReservations.jsx'
 
 function App() {
 
@@ -30,8 +32,8 @@ function App() {
 
     <Provider store={store}>
       <BrowserRouter>
+        <div className="app-container">
           <Nav />
-  
           <Routes>
 
             <Route path={reactRouter.user.homepage} element={<Home />} />
@@ -46,11 +48,14 @@ function App() {
             <Route path={reactRouter.admin.chooseEditHouse} element={<AdminEditHouseCard />} />
             <Route path={reactRouter.admin.login} element={<AdminLogin />} />
             <Route path={reactRouter.user.house} element={<HomesList />} />
+
+            <Route path={reactRouter.admin.editReservations} element={<AdminEditReservations />} />
+
             {localStorage.getItem('token') && <Route path={reactRouter.admin.logout} element={<AdminLogout />} />}
             <Route path={reactRouter.user.notfound} element={<NotFound />} />
 
           </Routes>
- 
+        </div>
       </BrowserRouter>
 
     </Provider>
