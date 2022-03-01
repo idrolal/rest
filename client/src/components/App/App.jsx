8cd7e9d48ab771ac6bcaccb6b51e79c2a34d0874
Victor Semin <syominvictor@gmail.com>
2022-03-01 10:24:38 +0300

    fix styles

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -32,10 +32,10 @@ function App() {
 
     <Provider store={store}>
       <BrowserRouter>
-          <Nav />
-        {/* <div className="app-container"> */}
-          <Routes>
 
+          <Nav />
+          
+          <Routes>
             <Route path={reactRouter.user.homepage} element={<Home />} />
             {/* <Route path={reactRouter.user.booking} element={<Booking />} /> */}
             <Route path={reactRouter.user.booking} element={<Booking2 />} />
@@ -48,14 +48,11 @@ function App() {
             <Route path={reactRouter.admin.chooseEditHouse} element={<AdminEditHouseCard />} />
             <Route path={reactRouter.admin.login} element={<AdminLogin />} />
             <Route path={reactRouter.user.house} element={<HomesList />} />
-
             <Route path={reactRouter.admin.editReservations} element={<AdminEditReservations />} />
-
             {localStorage.getItem('token') && <Route path={reactRouter.admin.logout} element={<AdminLogout />} />}
             <Route path={reactRouter.user.notfound} element={<NotFound />} />
-
           </Routes>
-        {/* </div> */}
+
       </BrowserRouter>
 
     </Provider>
--- client/src/components/Home/Questions/Questions.jsx
+++ client/src/components/Home/Questions/Questions.jsx
@@ -5,18 +5,18 @@ import right from '../../../img/right.svg'
 function Questions(props) {
   return (
     <>
-    <h3>частые вопросы</h3>
+    <h3>часто задаваемые вопросы</h3>
     <ul class="collapsible">
     <li>
-      <div class="collapsible-header"><img className='collapse-logo' src={right} alt="" /><span className='collapsible-head'>Бронирование:</span></div>
+      <div class="collapsible-header"><img className='collapse-logo' src={right} alt="" /><span className='collapsible-head'>Как осуществить бронь?</span></div>
       <div class="collapsible-body">
         <span className='collapse-text'>
-Бронирование шатров осуществляется только через модуль бронирования на нашем сайте по предоплате.
+Бронирование домиков осуществляется только через модуль бронирования на нашем сайте по предоплате.
 Бронирования являются невозвратными. При отмене бронирования средства 
 не возвращаются. Перенос на другие даты не возможен.
-Если у вас возникли вопросы, позвоните по номеру телефона +7 921 873 92 32.
+Если у вас возникли вопросы, позвоните по номеру телефона +7 921 877 97 37.
 Для того, чтобы разместить бронирование, выберете даты проживания 
-и тип шатра в модуле бронирования, укажите количество гостей. 
+и тип домика в модуле бронирования, укажите количество гостей. 
 Детей, которым не требуется отдельное спальное место, указывать 
 в бронировании не нужно. Нажмите на кнопку «забронировать», 
 введите свои данные: Ф.И.О., номер телефона, эл. почту. Выберете способ оплаты.
@@ -24,8 +24,12 @@ function Questions(props) {
 </div>
     </li>
     <li>
-      <div className="collapsible-header"><img className='collapse-logo' src={right} alt="" /><span className='collapsible-head'>Бронирование:</span></div>
-      <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      <div className="collapsible-header"><img className='collapse-logo' src={right} alt="" /><span className='collapsible-head'>Как добраться?</span></div>
+      <div className="collapsible-body">
+        <span className='collapse-text'>
+          Lorem ipsum dolor sit amet.
+          </span>
+          </div>
     </li>
     <li>
       <div className="collapsible-header"><img className='collapse-logo' src={right} alt="" /><span className='collapsible-head'>Бронирование:</span></div>
