fd3f493df4b13c665ae20e12a71539cebc814e8c
Anna <anna.si4eva3000@gmail.com>
2022-03-11 22:22:22 +0300

    a

--- server/db/seeders/20220227084128-admins.js
+++ server/db/seeders/20220227084128-admins.js
@@ -1,30 +1,12 @@
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up(queryInterface) {
     await queryInterface.bulkInsert('Admins', [{
       name: 'Andew',
       email: 'a@yandex.ru',
-      password: '12345678',
+      password: '123456789',
       isAdmin: true,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
-
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
   },
 };
