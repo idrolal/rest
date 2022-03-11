521ed3b39b56246d0cc7df0c917fbf743260354d
Anna <anna.si4eva3000@gmail.com>
2022-03-11 23:26:09 +0300

    b

--- server/db/seeders/20220227084128-admins.js
+++ server/db/seeders/20220227084128-admins.js
@@ -2,7 +2,7 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('Admins', [{
       name: 'Andew',
-      email: 'a@yandex.ru',
+      email: 'aa@yandex.ru',
       password: '123456789',
       isAdmin: true,
       createdAt: new Date(),
